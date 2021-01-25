9e7c5ad294d00f2686697b52e4b60c19a1729d23
GuillermoInfanteSMA <jose.infante@smags.com.mx>
2021-01-25 16:21:21 -0600

    #4, Se implementó función para que el usuario tenga el control sobre cómo le gustaría formar la serie de colores

--- src/baseandonwireless/BaseAndonWireless.java
+++ src/baseandonwireless/BaseAndonWireless.java
@@ -16,6 +16,7 @@ import com.pi4j.io.serial.StopBits;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  *
@@ -33,6 +34,17 @@ public class BaseAndonWireless {
     private static final int BODY_LENGTH = 5;
     private static final int FOOTER_LENGTH = 2;
     private static final int MESSAGE_LENGTH = 13;
+    //Bytes de colores que soporta la torreta
+    private static final byte turnOff = 0x00;
+    private static final byte RED = 0x01;
+    private static final byte AMBER = 0x02;
+    private static final byte LEMON = 0x03;
+    private static final byte GREEN = 0x04;
+    private static final byte SKY_BLUE = 0x05;
+    private static final byte BLUE = 0x06;
+    private static final byte PURPLE = 0x07;
+    private static final byte PINK = 0x08;
+    private static final byte WHITE = 0x09;
     
     public BaseAndonWireless(){
         this.Connect();
@@ -58,37 +70,66 @@ public class BaseAndonWireless {
         }catch(InterruptedException ie){ System.err.println("Error en Hilo (Sleep) dentro de conexión"); }
     } 
     
+    public byte [] seleccionColorSerie(){
+        byte [] byteReturn = new byte [5];
+        System.out.println("Ingrese número de 5 dígitos, cada digito corresponderá a un led de la torreta");
+        System.out.println("0-Apagar\t1-Rojo\t2-Ambar\t3-Limón\t4-Verde\n"
+                + "5-Azul Cielo\t6-Azul\t7-Morado\t8-Rosa\t9-Blanco");
+        System.out.print("Serie: ");
+        String resp = new Scanner(System.in).nextLine();
+        for(int i = 0; i < 5; i++){
+            switch(resp.charAt(i)){
+                case '0':
+                    byteReturn[i] = this.turnOff;
+                    break;
+                case '1':
+                    byteReturn[i] = this.RED;
+                    break;
+                case '2':
+                    byteReturn[i] = this.AMBER;
+                    break;
+                case '3':
+                    byteReturn[i] = this.LEMON;
+                    break;
+                case '4':
+                    byteReturn[i] = this.GREEN;
+                    break;
+                case '5':
+                    byteReturn[i] = this.SKY_BLUE;
+                    break;
+                case '6':
+                    byteReturn[i] = this.BLUE;
+                    break;
+                case '7':
+                    byteReturn[i] = this.PURPLE;
+                    break;
+                case '8':
+                    byteReturn[i] = this.PINK;
+                    break;
+                case '9':
+                    byteReturn[i] = this.WHITE;
+                    break;
+                default:
+                    byteReturn[i] = this.turnOff;
+                    break;
+            }
+        }
+        return byteReturn;
+    } 
+    
     public void loop(){
         Thread thread = new Thread(){
             @Override
             public void run() {
                 try {
-                    int auxContador = 0;
-                    int i = 0;
                     byte[] message;
-                    while(auxContador != 10){
-                        //out.write(MESSAGE_BODY[i]); //first attempt
-                        message = Arrays.copyOf(MESSAGE_HEADER, HEADER_LENGTH + BODY_LENGTH);
-                        System.arraycopy(MESSAGE_BODY[i], 0, message, HEADER_LENGTH, BODY_LENGTH);
-                        message = Arrays.copyOf(message, MESSAGE_LENGTH);
-                        System.arraycopy(MESSAGE_FOOTER, 0, message, HEADER_LENGTH + BODY_LENGTH, FOOTER_LENGTH);
-                        out.write(message);
-                        System.out.println("Cambiando de Color...");
-                        Thread.sleep(5000);
-                        i++;
-                        auxContador++;
-                        if(i == MESSAGE_BODY.length){
-                            i=0;
-                        }
-                    }
                     message = Arrays.copyOf(MESSAGE_HEADER, HEADER_LENGTH + BODY_LENGTH);
-                    System.arraycopy(ALL_TURNOFF, 0, message, HEADER_LENGTH, BODY_LENGTH);
+                    System.arraycopy(seleccionColorSerie(), 0, message, HEADER_LENGTH, BODY_LENGTH);
                     message = Arrays.copyOf(message, MESSAGE_LENGTH);
                     System.arraycopy(MESSAGE_FOOTER, 0, message, HEADER_LENGTH + BODY_LENGTH, FOOTER_LENGTH);
-                    System.out.println("Apangando Torreta...");
                     out.write(message);
+                    System.out.println("Cambiando de Color...");
                     Thread.sleep(5000);
-                    System.out.println("HILO: Terminó mi proceso.");
                 }catch( IOException ioe ) {System.err.println("Error al enviar mensaje: " + ioe);
                 }catch( InterruptedException ie ){System.err.println("Error dentro de Thread: " + ie);}
             }
